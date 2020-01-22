31902dc7b1d82d2a9f45d2aa2a17eb1cdefccc3e
alirezamah <alireza.mahzoon44@gmail.com>
2020-01-22 23:43:05 +0100

    practice ´tonight meeting subjects

---
+++ src/main/java/se/almstudio/collectionmanager/service/Test.java
@@ -0,0 +1,15 @@
+package se.almstudio.collectionmanager.service;
+
+public class Test {
+  /**
+   *
+   */
+  char [][] scores =
+    {
+      {'A','E','D','C','B','A'},
+      {'B','F','E','D','A','B'},
+      {'C','A','F','E','D','C'},
+      {'D','B','A','F','E','D'},
+      {'E','C','B','A','F','E'},
+    };
+}
